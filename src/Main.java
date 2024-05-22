0fb3cde68694de1d300836b36467aebf2ae0b156
Abigail Anyang <abigailanyang24@gmail.com>
2024-05-21 23:30:00 -0700

    second Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// USING RECURSION METHOD.
+public class Main {
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+            return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args){
+       int n = 7;
+        System.out.println(fibonacci(n));
+
+    }
+}
